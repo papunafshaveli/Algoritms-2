cd3771d3151d65de9ac6ed817a01a94154bdab6a
Papuna Fshaveli <papunafshaveli@gmail.com>
2023-10-12 21:38:46 +0400

    function  Removes all elements from the initial array that are of the same value as these arguments

---
+++ dublicateRemover.js
@@ -0,0 +1,24 @@
+/* Seek and Destroy
+You will be provided with an initial array as the first argument to 
+the destroyer function, followed by one or more arguments. Remove all 
+elements from the initial array that are of the same 
+value as these arguments.
+
+The function must accept an indeterminate number of arguments, also known 
+as a variadic function. You can access the additional arguments by adding a rest
+parameter to the function definition or using the arguments object. */
+
+function destroyer(arr, ...args) {
+  let argumentsArr = [];
+  for (let i = 0; i < args.length; i++) {
+    argumentsArr.push(args[i]);
+  }
+
+  let filteredArr = arr.filter((el) => {
+    return !argumentsArr.includes(el);
+  });
+
+  return filteredArr;
+}
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
